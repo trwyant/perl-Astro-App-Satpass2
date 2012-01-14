b1d24df3177190997ead559df63597c0033b4bee
tom <tom@f6fd3633-1913-47c2-bad4-372d75c948ec>
2012-01-14 21:41:42 +0000

    Update copyright to 2012.
    git-svn-id: file:///Users/tom/Code/svn/repos/Astro-App-Satpass2/trunk@2973 f6fd3633-1913-47c2-bad4-372d75c948ec

--- lib/Astro/App/Satpass2.pm
+++ lib/Astro/App/Satpass2.pm
@@ -2164,7 +2164,7 @@ sub version : Verb() {
 
 @{[__PACKAGE__]} $VERSION - Satellite pass predictor
 based on Astro::Coord::ECI @{[Astro::Coord::ECI->VERSION]}
-Copyright (C) 2009-2011 by Thomas R. Wyant, III
+Copyright (C) 2009-2012 by Thomas R. Wyant, III
 
 EOD
 }
@@ -6881,7 +6881,7 @@ Thomas R. Wyant, III (F<wyant at cpan dot org>)
 
 =head1 COPYRIGHT AND LICENSE
 
-Copyright (C) 2009-2011 by Thomas R. Wyant, III
+Copyright (C) 2009-2012 by Thomas R. Wyant, III
 
 This program is free software; you can redistribute it and/or modify it
 under the same terms as Perl 5.10.0. For more details, see the full text
