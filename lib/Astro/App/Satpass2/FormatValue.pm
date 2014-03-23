9314bf7af7860e646892081d378d67a2a24bc508
Tom Wyant <wyant@cpan.org>
2014-03-23 14:48:37 -0400

    Have magnitude formatter call magnitude().
    This happens only if the {magnitude} key is not found in the data and
    the {body} object has a magnitude() method. The method is passed the
    station onject.

--- lib/Astro/App/Satpass2/FormatValue.pm
+++ lib/Astro/App/Satpass2/FormatValue.pm
@@ -1273,7 +1273,23 @@ my %formatter_data = (	# For generating formatters
 	},
 	fetch	=> sub {
 	    my ( $self, $name, $arg ) = @_;
-	    return $self->_get( data => 'magnitude' );
+
+	    my $mag;
+	    defined( $mag = $self->_get( data => 'magnitude' ) )
+		and return $mag;
+
+	    my ( $body, $sta );
+	    $body = $self->_get_eci( 'body' )
+		and $body->can( 'magnitude' )
+		and $sta = $self->_get_eci( 'station' )
+		or return NONE;
+	    my $time;
+	    if ( defined( $time = $self->_get( data => 'time' ) ) ) {
+		$body->universal( $time );
+	    } elsif ( ! defined( $time = $body->universal() ) ) {
+		return NONE;
+	    }
+	    return $body->magnitude( $sta );
 	},
     },
 
@@ -3784,8 +3800,11 @@ C<9>.
 
  print $fmt->magnitude();
 
-This method formats the contents of C<{magnitude}>, which generally
-represent the magnitude of an Iridium flare.
+This method formats the contents of C<{data}{magnitude}>, which
+generally represent the magnitude of an Iridium flare. If C<{magnitude}>
+is undefined but the body supports the C<magnitude()> method, both body
+and station time are set to the contents of C<{time}>, and then
+C<< $body->magnitude( $station ) >> is called.
 
 In addition to the standard arguments, it takes the following:
 
