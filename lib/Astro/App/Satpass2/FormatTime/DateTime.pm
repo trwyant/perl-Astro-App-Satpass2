c282d7ff88c2088cc50ee76a00fa6aac5299089d
Tom Wyant <wyant@cpan.org>
2015-06-18 21:20:47 -0300

    Version 0.025_01.
    Set version to 0.025_01. Update Changes file for changes since 0.025.

--- lib/Astro/App/Satpass2/FormatTime/DateTime.pm
+++ lib/Astro/App/Satpass2/FormatTime/DateTime.pm
@@ -14,7 +14,7 @@ use Astro::App::Satpass2::Locale qw{ __preferred };
 use DateTime;
 use DateTime::TimeZone;
 
-our $VERSION = '0.025';
+our $VERSION = '0.025_01';
 
 sub format_datetime {
     my ( $self, $tplt, $time, $gmt ) = @_;
--- lib/Astro/App/Satpass2/FormatTime/DateTime/Strftime.pm
+++ lib/Astro/App/Satpass2/FormatTime/DateTime/Strftime.pm
@@ -14,7 +14,7 @@ use DateTime;
 use DateTime::TimeZone;
 use POSIX ();
 
-our $VERSION = '0.025';
+our $VERSION = '0.025_01';
 
 sub __format_datetime {
     my ( $self, $object, $tplt ) = @_;
