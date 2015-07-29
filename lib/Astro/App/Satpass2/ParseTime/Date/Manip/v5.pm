d9b07646ef772ebde9696ab43d30062e6b1d049c
Tom Wyant <wyant@cpan.org>
2015-07-28 21:40:10 -0400

    Version 0.027_01
    Set version to 0.027_01. Update Changes file for changes since 0.027.

--- lib/Astro/App/Satpass2/ParseTime/Date/Manip/v5.pm
+++ lib/Astro/App/Satpass2/ParseTime/Date/Manip/v5.pm
@@ -10,7 +10,7 @@ use base qw{ Astro::App::Satpass2::ParseTime::Date::Manip };
 
 use Astro::App::Satpass2::Utils qw{ load_package };
 
-our $VERSION = '0.027';
+our $VERSION = '0.027_01';
 
 my $invalid;
 
