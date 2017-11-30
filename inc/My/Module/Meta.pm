2705decf8d7e5087bd4236cae81ac95657e5c7e1
Tom Wyant <wyant@cpan.org>
2017-11-30 11:37:41 -0500

    Remove mailto: bug reporting metadata
    Not that I refuse these, it is just that it appears to cause RT to think
    I will not accept bug reports from them.

--- inc/My/Module/Meta.pm
+++ inc/My/Module/Meta.pm
@@ -89,7 +89,7 @@ sub meta_merge {
 	resources	=> {
 	    bugtracker	=> {
                 web	=> 'https://rt.cpan.org/Public/Dist/Display.html?Name=Astro-App-Satpass2',
-                mailto  => 'wyant@cpan.org',
+#                mailto  => 'wyant@cpan.org',
             },
 	    license	=> 'http://dev.perl.org/licenses/',
 	    repository	=> {
