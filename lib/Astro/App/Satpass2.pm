b5a7f50db472360853a71e2dd5a0e36fea6a7c95
Tom Wyant <wyant@cpan.org>
2018-01-26 11:03:54 -0500

    Version 0.035
    Set version to 0.035. Update Changes file for changes since v00.034_01.

--- lib/Astro/App/Satpass2.pm
+++ lib/Astro/App/Satpass2.pm
@@ -62,7 +62,7 @@ use constant NULL	=> bless \( my $x = undef ), 'Null';
 # NULL_REF eq ref $rslt
 use constant NULL_REF	=> ref NULL;
 
-our $VERSION = '0.034_01';
+our $VERSION = '0.035';
 
 # The following 'cute' code is so that we do not determine whether we
 # actually have optional modules until we really need them, and yet do
@@ -3076,7 +3076,7 @@ sub version : Verb() {
 
 @{[__PACKAGE__]} $VERSION - Satellite pass predictor
 based on Astro::Coord::ECI @{[Astro::Coord::ECI->VERSION]}
-Copyright (C) 2009-2017 by Thomas R. Wyant, III
+Copyright (C) 2009-2018 by Thomas R. Wyant, III
 
 EOD
 }
@@ -8503,7 +8503,7 @@ Thomas R. Wyant, III (F<wyant at cpan dot org>)
 
 =head1 COPYRIGHT AND LICENSE
 
-Copyright (C) 2009-2017 by Thomas R. Wyant, III
+Copyright (C) 2009-2018 by Thomas R. Wyant, III
 
 This program is free software; you can redistribute it and/or modify it
 under the same terms as Perl 5.10.0. For more details, see the full text
