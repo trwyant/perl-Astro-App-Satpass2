56aa6c6967bdc133cece3e709dc96d3a13004a08
Tom Wyant <wyant@cpan.org>
2019-07-05 09:36:51 -0400

    Version 0.039_01
    Set version to 0.039_01. Update Changes file for changes since v00.039.

--- lib/Astro/App/Satpass2/Format.pm
+++ lib/Astro/App/Satpass2/Format.pm
@@ -13,7 +13,7 @@ use Astro::App::Satpass2::Utils qw{
     @CARP_NOT
 };
 
-our $VERSION = '0.039';
+our $VERSION = '0.039_01';
 
 use constant DEFAULT_LOCAL_COORD => 'azel_rng';
 
--- lib/Astro/App/Satpass2/Wrap/Array.pm
+++ lib/Astro/App/Satpass2/Wrap/Array.pm
@@ -7,7 +7,7 @@ use warnings;
 
 use Astro::App::Satpass2::Utils qw{ ARRAY_REF @CARP_NOT };
 
-our $VERSION = '0.039';
+our $VERSION = '0.039_01';
 
 sub new {
     my ( $class, $array ) = @_;
