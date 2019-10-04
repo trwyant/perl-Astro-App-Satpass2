f914d202f13a75c3f44e787e86d10b7873e721d1
Tom Wyant <wyant@cpan.org>
2019-10-04 08:43:58 -0400

    Version 0.041
    Set version to 0.041. Update Changes file for changes since v00.040_01.

--- inc/My/Module/Recommend/Any.pm
+++ inc/My/Module/Recommend/Any.pm
@@ -8,7 +8,7 @@ use warnings;
 use Carp;
 use Exporter qw{ import };
 
-our $VERSION = '0.040_01';
+our $VERSION = '0.041';
 
 our @EXPORT_OK = qw{ __any };
 
--- lib/Astro/App/Satpass2/Format/Template.pm
+++ lib/Astro/App/Satpass2/Format/Template.pm
@@ -27,7 +27,7 @@ use Template::Provider;
 use Text::Abbrev;
 use Text::Wrap qw{ wrap };
 
-our $VERSION = '0.040_01';
+our $VERSION = '0.041';
 
 sub new {
     my ($class, @args) = @_;
