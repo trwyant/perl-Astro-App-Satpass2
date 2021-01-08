4551e5774217ebd9163b0bc34e81117dd1438202
Tom Wyant <wyant@cpan.org>
2021-01-08 09:33:56 -0500

    Version 0.046
    Set version to 0.046. Update Changes file for changes since v00.045_01.

--- lib/Astro/App/Satpass2/Format/Dump.pm
+++ lib/Astro/App/Satpass2/Format/Dump.pm
@@ -11,7 +11,7 @@ use Astro::App::Satpass2::Utils qw{
     @CARP_NOT
 };
 
-our $VERSION = '0.045_01';
+our $VERSION = '0.046';
 
 my %dumper_hash = (
     'YAML' => 'Dump',
--- lib/Astro/App/Satpass2/Format/Template.pm
+++ lib/Astro/App/Satpass2/Format/Template.pm
@@ -27,7 +27,7 @@ use Template::Provider;
 use Text::Abbrev;
 use Text::Wrap qw{ wrap };
 
-our $VERSION = '0.045_01';
+our $VERSION = '0.046';
 
 sub new {
     my ($class, @args) = @_;
