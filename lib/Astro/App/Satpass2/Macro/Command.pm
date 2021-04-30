5be1b5ab4185e039ccf0b30500f4ceba885b32c7
Tom Wyant <wyant@cpan.org>
2021-04-30 11:09:21 -0400

    Version 0.047_001 (not released to CPAN)
    Set version to 0.047_001. Update Changes file for changes since v00.047.

--- lib/Astro/App/Satpass2/Macro/Command.pm
+++ lib/Astro/App/Satpass2/Macro/Command.pm
@@ -9,7 +9,7 @@ use parent qw{ Astro::App::Satpass2::Macro };
 
 use Astro::App::Satpass2::Utils qw{ quoter ARRAY_REF @CARP_NOT };
 
-our $VERSION = '0.047';
+our $VERSION = '0.047_001';
 
 sub execute {
     my ( $self, $name ) = @_;
--- lib/Astro/App/Satpass2/ParseTime/Date/Manip.pm
+++ lib/Astro/App/Satpass2/ParseTime/Date/Manip.pm
@@ -13,7 +13,7 @@ use Astro::App::Satpass2::Utils qw{
     @CARP_NOT
 };
 
-our $VERSION = '0.047';
+our $VERSION = '0.047_001';
 
 sub __class_name {
     return __PACKAGE__;
