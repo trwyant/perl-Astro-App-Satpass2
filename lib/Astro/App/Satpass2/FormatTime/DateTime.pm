87ffbb70aab7f7736d55a368275fd7f4e4e80799
Tom Wyant <wyant@cpan.org>
2022-08-25 15:46:07 -0400

    Version 0.049_002 (not released to CPAN)
    Set version to 0.049_002. Update Changes file for changes since v00.049_001.

--- lib/Astro/App/Satpass2/FormatTime/DateTime.pm
+++ lib/Astro/App/Satpass2/FormatTime/DateTime.pm
@@ -19,7 +19,7 @@ use Astro::App::Satpass2::Locale qw{ __preferred };
 use DateTime;
 use DateTime::TimeZone;
 
-our $VERSION = '0.049_001';
+our $VERSION = '0.049_002';
 
 sub attribute_names {
     my ( $self ) = @_;
--- lib/Astro/App/Satpass2/ParseTime/ISO8601.pm
+++ lib/Astro/App/Satpass2/ParseTime/ISO8601.pm
@@ -12,7 +12,7 @@ use Astro::Coord::ECI::Utils 0.112 qw{
 
 use parent qw{ Astro::App::Satpass2::ParseTime };
 
-our $VERSION = '0.049_001';
+our $VERSION = '0.049_002';
 
 my $package = __PACKAGE__;
 
