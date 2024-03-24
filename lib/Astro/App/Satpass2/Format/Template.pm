0b62a3e362b8db91063585fc52284b89e4db1979
Tom Wyant <wyant@cpan.org>
2024-03-24 17:42:02 -0400

    Version 0.053_02
    Set version to 0.053_02. Update Changes file for changes since v00.053_01.

--- lib/Astro/App/Satpass2/Format/Template.pm
+++ lib/Astro/App/Satpass2/Format/Template.pm
@@ -27,7 +27,7 @@ use Template::Provider;
 use Text::Abbrev;
 use Text::Wrap qw{ wrap };
 
-our $VERSION = '0.053_01';
+our $VERSION = '0.053_02';
 
 use constant FORMAT_VALUE	=> 'Astro::App::Satpass2::FormatValue';
 
--- lib/Astro/App/Satpass2/Warner.pm
+++ lib/Astro/App/Satpass2/Warner.pm
@@ -8,7 +8,7 @@ use warnings;
 use Astro::App::Satpass2::Locale qw{ __message };
 use Astro::App::Satpass2::Utils qw{ @CARP_NOT };
 
-our $VERSION = '0.053_01';
+our $VERSION = '0.053_02';
 
 sub new {
     my ( $class, @arg ) = @_;
